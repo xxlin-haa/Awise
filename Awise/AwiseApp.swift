b9b89164abd105799341311edb8c79f298531639
Sizchode <100533169+Sizchode@users.noreply.github.com>
2022-10-02 15:56:35 -0400

    Initial Commit

---
+++ Awise/AwiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwiseApp.swift
+//  Awise
+//
+//  Created by liu zhenke on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct AwiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
