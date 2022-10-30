c7098317e50b550016a80f67edcc660917cf9393
Sizchode <100533169+Sizchode@users.noreply.github.com>
2022-10-30 17:30:16 -0400

    Create settingview.swift

---
+++ settingview.swift
@@ -0,0 +1,31 @@
+//
+//  setting view.swift
+//  Awise
+//
+//  Created by liu zhenke on 10/24/22.
+//
+
+import SwiftUI
+
+struct Setting_Previews: PreviewProvider {
+     static var previews: some View {
+        SettingView()
+    }
+}
+
+struct SettingView: View{
+    var body: some View {
+        Form{
+            Section(header: Text("Setting"),footer:Text("")){
+                
+                Toggle(isOn:.constant(true),
+                       label:{
+                    Text("Dark mode")
+                })
+                Toggle(isOn: .constant(true), label: {
+                    Text("Tinder Recommendation")
+                })
+            }
+        }
+    }
+}
